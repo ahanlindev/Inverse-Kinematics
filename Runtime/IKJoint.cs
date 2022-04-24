0c1b64918b2d25e33c7acb45b1631297eb75365d
Alex Hanlin <alexhanlin@utexas.edu>
2022-04-24 12:59:04 -0500

    Stripped down IKJoint. Will be built out later.

--- Runtime/IKJoint.cs
+++ Runtime/IKJoint.cs
@@ -6,30 +6,16 @@ namespace ahanlindev
 {
     public class IKJoint : MonoBehaviour
     {
-        [SerializeField] private HashSet<IKJoint> childBones;
         // Start is called before the first frame update
         void Start()
         {
-            childBones = new HashSet<IKJoint>();
-            updateChildBones();
+
         }
 
         // Update is called once per frame
         void Update()
         {
         
-        }
-
-        private void updateChildBones()
-        {
-            foreach (Transform child in transform)
-            {
-                IKJoint childBone = child.GetComponent<IKJoint>();
-                if (childBone != null && !child.Equals(transform))
-                {
-                    childBones.Add(childBone);
-                }
-            }
         }
     }
 }
